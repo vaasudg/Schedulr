7ccd7fc70ec76d906aa6c59db3804e70540b02a1
Vaasu <vaasu.dg@gmail.com>
2023-03-27 14:05:55 +0530

    image unoptimized

--- next.config.js
+++ next.config.js
@@ -2,9 +2,15 @@
 const nextConfig = {
   dangerouslyAllowSVG: true,
   images: {
+    loader: 'imgix',
     domains: ['source.unsplash.com', 'image.tmdb.org'],
   },
   reactStrictMode: true,
+  experimental: {
+    images: {
+        unoptimized: true
+    }
+  }
 }
 
 module.exports = nextConfig
